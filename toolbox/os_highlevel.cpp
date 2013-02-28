7578fbce7388229c6364a4ebb16f55c8ea9ad471
Kelvin Sherlock <ksherlock@gmail.com>
2013-02-28 18:58:37 -0500

    getfinfo / setfinfo

--- toolbox/os_highlevel.cpp
+++ toolbox/os_highlevel.cpp
@@ -119,10 +119,18 @@ namespace OS {
 	{
 		// FSMakeFSSpec(vRefNum: Integer; dirID: LongInt; fileName: Str255; VAR spec: FSSpec): OSErr;
 
+		/*
+		 * See Chapter 2, File Manager / Using the File Manager, 2-35
+		 *
+		 *
+		 */
+
 		uint16_t vRefNum;
 		uint32_t dirID;
 		uint32_t fileName;
 		uint32_t spec;
+
+
 
 
 		StackFrame<14>(vRefNum, dirID, fileName, spec);
@@ -149,6 +157,8 @@ namespace OS {
 
 			path.assign(cp);
 
+			// if sname is null then the target is the default directory... 
+			// so this should be ok.
 
 			int pos = path.find_last_of('/');
 			if (pos == path.npos)
@@ -182,6 +192,99 @@ namespace OS {
 		return 0;
 	}
 
+
+	uint16_t FSpGetFInfo()
+	{
+		// FSpGetFInfo (spec: FSSpec; VAR fndrInfo: FInfo): OSErr;
+
+		uint32_t spec;
+		uint32_t finderInfo;
+
+		StackFrame<8>(spec, finderInfo);
+
+		int parentID = memoryReadLong(spec + 2);
+
+		std::string leaf = ToolBox::ReadPString(spec + 6, false);
+		std::string path = FSSpecManager::pathForID(parentID);
+
+		path += leaf;
+
+		Log("     FSpGetFInfo(%s, %08x)\n",  path.c_str(), finderInfo);
+
+
+
+		// todo -- move to separate function? used in multiple places.
+		uint8_t buffer[32];
+		std::memset(buffer, 0, sizeof(buffer));
+		int rv;
+
+		rv = ::getxattr(path.c_str(), XATTR_FINDERINFO_NAME, buffer, 32, 0, 0);
+
+		if (rv < 0)
+		{
+			switch (errno)
+			{
+				case ENOENT:
+				case EACCES:
+					return errno_to_oserr(errno);
+			}
+		}
+		// override for source files.
+		if (IsTextFile(path))
+		{
+			std::memcpy(buffer, "TEXTMPS ", 8);
+		}		
+
+		std::memmove(memoryPointer(finderInfo), buffer, 16);
+		return 0;
+	}
+
+	uint16_t FSpSetFInfo()
+	{
+		// FSpSetFInfo (spec: FSSpec; VAR fndrInfo: FInfo): OSErr;
+
+		uint32_t spec;
+		uint32_t finderInfo;
+
+		StackFrame<8>(spec, finderInfo);
+
+		int parentID = memoryReadLong(spec + 2);
+
+		std::string leaf = ToolBox::ReadPString(spec + 6, false);
+		std::string path = FSSpecManager::pathForID(parentID);
+
+		path += leaf;
+
+		Log("     FSpSetFInfo(%s, %08x)\n",  path.c_str(), finderInfo);
+
+
+
+		// todo -- move to separate function? used in multiple places.
+		uint8_t buffer[32];
+		std::memset(buffer, 0, sizeof(buffer));
+		int rv;
+
+		rv = ::getxattr(path.c_str(), XATTR_FINDERINFO_NAME, buffer, 32, 0, 0);
+
+		if (rv < 0)
+		{
+			switch (errno)
+			{
+				case ENOENT:
+				case EACCES:
+					return errno_to_oserr(errno);
+			}
+		}
+
+		std::memmove(buffer, memoryPointer(finderInfo), 16);
+
+		rv = ::setxattr(path.c_str(), XATTR_FINDERINFO_NAME, buffer, 32, 0, 0);
+
+
+		return rv < 0 ? errno_to_oserr(errno) : 0;
+	}
+
+
 	uint16_t HighLevelHFSDispatch(uint16_t trap)
 	{
 
@@ -194,6 +297,14 @@ namespace OS {
 		{
 			case 0x0001:
 				return FSMakeFSSpec();
+				break;
+
+			case 0x0007:
+				return FSpGetFInfo();
+				break;
+
+			case 0x0008:
+				return FSpSetFInfo();
 				break;
 
 			default:
