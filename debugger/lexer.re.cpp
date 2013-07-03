98b54555499ab8a27a67c3eda9423d291f3084f9
Kelvin Sherlock <ksherlock@gmail.com>
2013-07-03 19:55:09 -0400

    fix parser bugs

--- debugger/lexer.re.cpp
+++ debugger/lexer.re.cpp
@@ -17,6 +17,7 @@ extern "C" {
 	void *ParseAlloc(void *(*mallocProc)(size_t));
 	void ParseFree(void *p, void (*freeProc)(void*));
 	void Parse(void *yyp, int yymajor, uint32_t yyminor, Command *command);
+	void ParseTrace(FILE *TraceFILE, char *zTracePrompt);
 
 }
 
@@ -37,7 +38,7 @@ namespace {
 	uint32_t scan10(const char *begin, const char *end)
 	{
 		return std::accumulate(begin, end, 0, 
-			[](char c, uint32_t value){
+			[](uint32_t value, char c){
 				return value * 10 + c - '0';
 			});
 	}
@@ -45,7 +46,7 @@ namespace {
 	uint32_t scan16(const char *begin, const char *end)
 	{
 		return std::accumulate(begin, end, 0, 
-			[](char c, uint32_t value){
+			[](uint32_t value, char c){
 				return (value << 4) + tox(c);
 			});
 	}
@@ -109,6 +110,8 @@ bool ParseLine(const char *iter, Command *command)
 
 	parser = ParseAlloc(malloc);
 
+	//ParseTrace(stdout, "--> ");
+
 	for (;;)
 	{
 		const char *begin = iter;
@@ -125,42 +128,42 @@ bool ParseLine(const char *iter, Command *command)
 			continue;
 		}
 
-		'>=' { Parse(&parser, tkGTEQ, 0, command); continue; }
-		'>>' { Parse(&parser, tkGTGT, 0, command); continue; }
-		'<=' { Parse(&parser, tkLTEQ, 0, command); continue; }
-		'<<' { Parse(&parser, tkLTLT, 0, command); continue; }
-		'!=' { Parse(&parser, tkBANGEQ, 0, command); continue; }
-		'==' { Parse(&parser, tkEQEQ, 0, command); continue; }
-		'||' { Parse(&parser, tkPIPEPIPE, 0, command); continue; }
-		'&&' { Parse(&parser, tkAMPAMP, 0, command); continue; }
+		'>=' { Parse(parser, tkGTEQ, 0, command); continue; }
+		'>>' { Parse(parser, tkGTGT, 0, command); continue; }
+		'<=' { Parse(parser, tkLTEQ, 0, command); continue; }
+		'<<' { Parse(parser, tkLTLT, 0, command); continue; }
+		'!=' { Parse(parser, tkBANGEQ, 0, command); continue; }
+		'==' { Parse(parser, tkEQEQ, 0, command); continue; }
+		'||' { Parse(parser, tkPIPEPIPE, 0, command); continue; }
+		'&&' { Parse(parser, tkAMPAMP, 0, command); continue; }
 
 
-		'(' { Parse(&parser, tkLPAREN, 0, command); continue; }
-		')' { Parse(&parser, tkRPAREN, 0, command); continue; }
-		'=' { Parse(&parser, tkEQ, 0, command); continue; }
-		'+' { Parse(&parser, tkPLUS, 0, command); continue; }
-		'-' { Parse(&parser, tkMINUS, 0, command); continue; }
-		'*' { Parse(&parser, tkSTAR, 0, command); continue; }
-		'/' { Parse(&parser, tkSLASH, 0, command); continue; }
-		'%' { Parse(&parser, tkPERCENT, 0, command); continue; }
-		'~' { Parse(&parser, tkTILDE, 0, command); continue; }
-		'!' { Parse(&parser, tkBANG, 0, command); continue; }
-		'^' { Parse(&parser, tkCARET, 0, command); continue; }
-		'&' { Parse(&parser, tkAMP, 0, command); continue; }
-		'|' { Parse(&parser, tkPIPE, 0, command); continue; }
-		'<' { Parse(&parser, tkLT, 0, command); continue; }
-		'>' { Parse(&parser, tkGT, 0, command); continue; }
+		'(' { Parse(parser, tkLPAREN, 0, command); continue; }
+		')' { Parse(parser, tkRPAREN, 0, command); continue; }
+		'=' { Parse(parser, tkEQ, 0, command); continue; }
+		'+' { Parse(parser, tkPLUS, 0, command); continue; }
+		'-' { Parse(parser, tkMINUS, 0, command); continue; }
+		'*' { Parse(parser, tkSTAR, 0, command); continue; }
+		'/' { Parse(parser, tkSLASH, 0, command); continue; }
+		'%' { Parse(parser, tkPERCENT, 0, command); continue; }
+		'~' { Parse(parser, tkTILDE, 0, command); continue; }
+		'!' { Parse(parser, tkBANG, 0, command); continue; }
+		'^' { Parse(parser, tkCARET, 0, command); continue; }
+		'&' { Parse(parser, tkAMP, 0, command); continue; }
+		'|' { Parse(parser, tkPIPE, 0, command); continue; }
+		'<' { Parse(parser, tkLT, 0, command); continue; }
+		'>' { Parse(parser, tkGT, 0, command); continue; }
 
 		[0-9]+ {
 			// integer
 			uint32_t data;
 
 			data = std::accumulate(begin, iter, 0,
-				[](char c, uint32_t value){
-					return value * 10 + c - '0';
+				[](uint32_t value, char c){
+					return (value * 10) + c - '0';
 			});
 
-			Parse(&parser, tkINTEGER, data, command);
+			Parse(parser, tkINTEGER, data, command);
 			continue;
 		}
 
@@ -169,11 +172,11 @@ bool ParseLine(const char *iter, Command *command)
 			uint32_t data;
 
 			data = std::accumulate(begin + 1, iter, 0,
-				[](char c, uint32_t value){
+				[](uint32_t value, char c){
 					return (value << 4) + tox(c);
 				}
 			);
-			Parse(&parser, tkINTEGER, data, command);
+			Parse(parser, tkINTEGER, data, command);
 			continue;
 		}
 
@@ -182,11 +185,11 @@ bool ParseLine(const char *iter, Command *command)
 			uint32_t data;
 
 			data = std::accumulate(begin + 2, iter, 0,
-				[](char c, uint32_t value){
+				[](uint32_t value, char c){
 					return (value << 4) + tox(c);
 				}
 			);
-			Parse(&parser, tkINTEGER, data, command);
+			Parse(parser, tkINTEGER, data, command);
 			continue;
 		}
 
@@ -195,12 +198,12 @@ bool ParseLine(const char *iter, Command *command)
 			uint32_t data;
 
 			data = std::accumulate(begin + 1, iter - 1, 0,
-				[](char c, uint32_t value)
+				[](uint32_t value, char c)
 				{
 					return (value << 8) + (unsigned)c;
 				}
 			);
-			Parse(&parser, tkINTEGER, data, command);
+			Parse(parser, tkINTEGER, data, command);
 			continue;
 		}
 
@@ -208,102 +211,108 @@ bool ParseLine(const char *iter, Command *command)
 		'd' [0-7] {
 			// data register
 			uint32_t data = begin[1] - '0';
-			Parse(&parser, tkDREGISTER, data, command);
+			Parse(parser, tkDREGISTER, data, command);
 			continue;
 		}
 
 		'a' [0-7] {
 			// address register
 			uint32_t data = begin[1] - '0';
-			Parse(&parser, tkAREGISTER, data, command);
+			Parse(parser, tkAREGISTER, data, command);
 			continue;
 		}
 
 		'pc' {
 			// program counter...
-			Parse(&parser, tkXREGISTER, 0, command);
+			Parse(parser, tkXREGISTER, 0, command);
 			continue;
 		}
 
 		'csr' {
 			// condition status register.
-			Parse(&parser, tkXREGISTER, 1, command);
+			Parse(parser, tkXREGISTER, 1, command);
 			continue;
 		}
 
 		'sp' {
 			// stack pointer aka a7
-			Parse(&parser, tkAREGISTER, 7, command);
+			Parse(parser, tkAREGISTER, 7, command);
 			continue;
 		}
 
 		'fp' {
 			// frame pointer aka a6
-			Parse(&parser, tkAREGISTER, 6, command);
+			Parse(parser, tkAREGISTER, 6, command);
 			continue;
 		}
 
 
 		'c' | 'continue' {
-			Parse(&parser, tkCONTINUE, 0, command);
+			Parse(parser, tkCONTINUE, 0, command);
 			continue;
 		}
 
 		'hd' | 'dump' {
-			Parse(&parser, tkDUMP, 0, command);
+			Parse(parser, tkDUMP, 0, command);
 			continue;
 		}
 
 		'h' | 'help' {
-			Parse(&parser, tkHELP, 0, command);
+			Parse(parser, tkHELP, 0, command);
 			continue;
 		}
 
 		'n' | 'next' {
-			Parse(&parser, tkNEXT, 0, command);
+			Parse(parser, tkNEXT, 0, command);
 			continue;
 		}
 
 		's' | 'step' {
-			Parse(&parser, tkNEXT, 0, command);
+			Parse(parser, tkNEXT, 0, command);
 			continue;
 		}
 
 		'b' | 'brk' | 'break' {
-			Parse(&parser, tkBREAK, 0, command);
+			Parse(parser, tkBREAK, 0, command);
 			continue;
 		}
 
 		'g' | 'go' {
-			Parse(&parser, tkCONTINUE, 0, command);
+			Parse(parser, tkCONTINUE, 0, command);
 			continue;
 		}
 
+		'p' | 'print' {
+			Parse(parser, tkPRINT, 0, command);
+			continue;
+		}
+	
 		'r' | 'run' {
-			Parse(&parser, tkCONTINUE, 0, command);
+			Parse(parser, tkCONTINUE, 0, command);
 			continue;
 		}
 
 
 		';l' | ';list' {
-			Parse(&parser, tkSEMIL, 0, command);
+			Parse(parser, tkSEMIL, 0, command);
 			continue;
 		}
 
 		';h' | ';hd' | ';hexdump' {
-			Parse(&parser, tkSEMIH, 0, command);
+			Parse(parser, tkSEMIH, 0, command);
 			continue;
 		}
 
 		[_A-Za-z][_A-Za-z0-9] + {
 			// identifier. lookup global address, tool number, etc.
-			fprintf(stderr, "illegal identifier: %*.s\n", (int)(iter - begin), begin);
+			fprintf(stderr, "illegal identifier: `%.*s`\n", (int)(iter - begin), begin);
 			return false;
 		}
 
 
 		[\x00] {
 			// eol.
+			Parse(parser, tkEOL, 0, command);
 			break;
 		}
 
@@ -316,8 +325,8 @@ bool ParseLine(const char *iter, Command *command)
 
 	}
 
-	Parse(&parser, 0, 0, command);
-	ParseFree(&parser, free);
+	Parse(parser, 0, 0, command);
+	ParseFree(parser, free);
 
 	return command->valid;
 }
